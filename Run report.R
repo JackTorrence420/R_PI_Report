035b708856358605acdd6e691bac5d7800e98a4c
JackTorrence420 <31537433+JackTorrence420@users.noreply.github.com>
2017-09-04 08:59:15 -0400

    removed params

--- Run report.R
+++ Run report.R
@@ -6,26 +6,26 @@ sink(paste0(sub('\\..*', '', 'PIReport'), format(Sys.time(),'_%Y%m%d_%H%M%S'), '
 
    for (csv in dir(pattern = ".csv$"))
    {
-     reportParams =ReportParameters$new(dataFile=csv, showOriginal = TRUE,extractedTextLength=200L, instancesToShow=2L,  wrapTextWidth=80L )
+     reportParams =ReportParameters$new(dataFile=csv, showOriginal = TRUE,extractedTextLength=100L, instancesToShow=100L )
      report<-Report$new(reportParams)
      report$printTitle()
      report$printTotals()
      report$printExamples()
    }
 
-#test 1 report
+# Uncomment these to test 1 report
 # reportParams =
 #   ReportParameters$new(dataFile="Copy of DeepSee_Diaceutics_20170208_20170228_v1.csv",
-#                        showOriginal = FALSE,
+#                        showOriginal = TRUE,
 #                        extractedTextLength=100L,
-#                        instancesToShow=100L,
-#                        wrapTextWidth=80L)
+#                        instancesToShow=100L
+#                        )
 # report<-Report$new(reportParams)
 # report$printTitle()
 # report$printTotals()
 # report$printExamples()
-
-sink()
+# 
+# sink()
 
 
 
